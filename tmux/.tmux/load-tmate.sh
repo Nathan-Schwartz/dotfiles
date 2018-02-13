fa265225664ea7562ad8a71126d8e648ab60fc7f
Nathan Schwartz <nathan.schwartz95@gmail.com>
2018-02-12 21:26:33 -0600

    Dynamically load tmux config

---
+++ tmux/.tmux/load-tmate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+verify_tmux_version () {
+  tmux_home=~/.tmux
+
+  case "$OSTYPE" in
+    darwin*) isMac=true ;;
+    *) isMac=false ;;
+  esac
+
+  if [ "$isMac" = true ] ; then
+    tmux source-file "$tmux_home/.tmate.mac.conf"
+    exit
+  else
+    tmux source-file "$tmux_home/.tmate.linux.conf"
+    exit
+  fi
+}
+
+verify_tmux_version
