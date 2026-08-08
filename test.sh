2c7591f1c242d069507f0a2db3a5559b5233c4e9
Nathan Schwartz <nathan.schwartz95@gmail.com>
2026-08-07 21:36:56 -0500

    sprintboard: add acli via mise with test.sh assertion

--- test.sh
+++ test.sh
@@ -37,6 +37,7 @@ assert "$(command_exists jc)" "true" 'jc not installed'
 assert "$(command_exists tree)" "true" 'tree not installed'
 assert "$(command_exists pipx)" "true" 'pipx not installed'
 assert "$(command_exists tk)" "true" 'tk not installed'
+assert "$(command_exists acli)" "true" 'acli not installed'
 
 echo "Running sprintboard tests..."
 node --test sprintboard/test/*.test.js
